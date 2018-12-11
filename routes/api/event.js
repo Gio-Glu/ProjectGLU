7db33b0673723ed0d445c4a5e4aeeb77eff0604c
Gio-Glu <42942538+Gio-Glu@users.noreply.github.com>
2018-12-11 20:09:37 +0100

    Add files via upload

--- routes/api/event.js
+++ routes/api/event.js
@@ -1,17 +1,17 @@
 var keystone = require('keystone');
 var Event = keystone.List('Event');
-var newEvent = new Event.model(req.body);
+
 module.exports = function (req, res) {
   if (!req.body.name || !req.body.startTime || !req.body.endTime) {
     return res.send({ status: 'incomplete data set' });
   }
 
+  var newEvent = new Event.model();
+  Event.updateItem(newEvent, req.body);
 
-
+  Event.updateItem(newEvent, req.body, function (error) {
+    res.locals.enquirySubmitted = true;
+    if (error) res.locals.saveError = true;
+    res.render('addEvent');
+  });
 };
-
-Event.updateItem(newEvent, req.body, function (error) {
-  res.locals.enquirySubmitted = true;
-  if (error) res.locals.saveError = true;
-  res.render('addEvent');
-});
