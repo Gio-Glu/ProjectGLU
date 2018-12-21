96528de8f4889892cbf3b927b914c5bc38cccf82
Gio-Glu <42942538+Gio-Glu@users.noreply.github.com>
2018-12-21 16:05:13 +0100

    Add files via upload

--- keystone.js
+++ keystone.js
@@ -37,14 +37,23 @@ keystone.init({
 
 	'chartbeat property': process.env.CHARTBEAT_PROPERTY,
 	'chartbeat domain': process.env.CHARTBEAT_DOMAIN,
-	'session-store': 'mongo',
-	
+	"sessionStore":{
+		"db": {
+		  "name": "myDb",
+		  "servers": [
+			{ "host": "192.168.1.100", "port": 28001 },
+			{ "host": "192.168.1.100", "port": 28002 },
+			{ "host": "192.168.1.101", "port": 27017 }
+		  ]
+		}
+	  }
+
 
 });
 
 keystone.import('models');
 
-keystone.get('session options');
+
 
 
 keystone.set('locals', {
